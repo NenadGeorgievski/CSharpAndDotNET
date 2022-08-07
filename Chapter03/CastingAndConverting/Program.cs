2940661b69721b47a07f7f4d815dd2f7d138f360
NenadGeorgievski <nenad.georgievski.ku@hotmail.com>
2022-08-07 23:03:34 +0200

    Working with casting and converting

---
+++ Chapter03/CastingAndConverting/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+using static System.Convert;
+
+int a = 10;
+double b = a; //an int can be safely converted to a double
+WriteLine("{0} is of type {1}", b, b.GetType());
+
+double c = 9.8;
+int d = (int)c; //the compiler gives an error for converting a double to int
+
+WriteLine("{0} is of type {1}", d, d.GetType());
+
+double g = 9.8;
+int h = ToInt32(g);
+WriteLine($"g is {g} and h is {h}");
+
+double[] doubles = new[] { 9.49, 9.88, 4.22, 233.2 };
+
+foreach (double n in doubles)
+{
+    WriteLine($"ToInt32({n}) is {ToInt32(n)}");
+}
---
+++ Chapter03/Iteration statements/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+
+int x = 0;
+
+while (x<10)
+{
+    WriteLine(x);
+    x++;
+}
+
+string? password;
+
+do
+{
+    Write("Please enter a password:");
+    password = ReadLine();
+} while (password != "Pa$$w0rd");
+
+
+WriteLine("Correct!");
+for (int i = 1; i <= 10; i++)
+{
+    WriteLine(i);
+}
+
+string[] names = { "Andrew", "James", "Charlie", "Trpe" };
+foreach (string name in names)
+{
+    WriteLine($"{name} has {name.Length} characters.");
+}
